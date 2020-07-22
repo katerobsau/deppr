d8d3f8323ca5b266af9708ea167f11c6ab59720b
Kate Saunders <katerobinsonsaunders@gmail.com>
2020-07-22 13:03:27 +0200

    Documentation for backward selection
    * started not finished

--- R/min_div_single_backward_selection.R
+++ R/min_div_single_backward_selection.R
@@ -1,6 +1,48 @@
-single_backward_selection_mindiv <- function(backward_num, X_mat, H_mat){
+#' Backward Selection for Minimum Divergence Schaake Shuffle
+#'
+#' @description Does stuff
+#'
+#' @param X_mat a matrix where rows correspond to different locations and lead times
+#' for a given ensemble forecast. The columns correspond to the number of members.
+#' @param H_mat a matrix of historical observations. The rows and columns of this
+#' matrix are arranged in such a way that the observational trajectories are
+#' consistent with the locations and lead times of the rows in \code{X_mat}.
+#' @param backward_num the number of dates to retain after backward selection
+#'
+#' @details details about stuff
+#'
+#' @return A reduced version of the matrix for \code{H_mat}
+#' with \code{backward_num} columns.
+#'
+#' @seealso \code{\link{get_mindiv_epsilon}}
+#'
+#' @author Kate Saunders and Kirien Whan
+#'
+#' @references
+#' Scheuerer, Michael, et al.
+#' "A method for preferential selection of dates in the
+#' Schaake shuffle approach to constructing spatiotemporal
+#' forecast fields of temperature and precipitation."
+#' Water Resources Research 53.4 (2017): 3029-3046.
+#'
+#' @examples
+#'
+#' @export
+#'
+#' X = matrix(rnorm(100), nrow = 2)
+#' H = matrix(rnorm(40), nrow = 2)
+#' backward_selection_mindiv(backward_num = 19, X_Mat = X, H_mat = H)
+backward_selection_mindiv <- function(backward_num, X_mat, H_mat){
 
   total_div_minus_j = rep(NA, ncol(H_mat))
+
+  # cl <- makeCluster(detectCores() - 1)
+  # doParallel::registerDoParallel(cl)
+  #
+  # foreach(j = 1:ncol(H_mat), .packages = c("depPPR")) %dopar% {
+  #   total_div_minus_j[j] <- get_total_divergence(X_mat = X_mat, H_mat = H_mat[,-j])
+  # }
+
   for(j in 1:ncol(H_mat))
     total_div_minus_j[j] <- get_total_divergence(X_mat = X_mat, H_mat = H_mat[,-j])
 
