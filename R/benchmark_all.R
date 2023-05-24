3bfb547b55b28eac659b6286062d92cb7870a2f5
Kate Saunders <kate.saunders@monash.edu>
2023-05-24 14:50:18 +1000

    pseudo coding the benchmark_all function
    Just thinking about what should be in this

--- R/benchmark_all.R
+++ R/benchmark_all.R
@@ -1,3 +1,20 @@
+
 benchmark_all <- function(){
+
+# Schaake
+
+# (Window / climate) Schaake
+
+# SimSchaake
+
+# MinDiv (this needs to be optional as its slow)
+
+# ECC
+
+# Need to think about what we want to return from here
+
+# eg. Return post-processed forecasts
+
+# Scoring the forecasts should be separate ??
 
 }
