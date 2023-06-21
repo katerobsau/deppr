4da9b6f472589f42da269dae1dffcde9423d170a
Kiri Whan <whan@knmi.nl>
2023-06-21 12:59:23 +0200

    splitting example script into univariate and reshuffling

--- helpers/script_example_multivariate.R
+++ helpers/script_example_multivariate.R
@@ -6,7 +6,6 @@
 library(tidyverse)
 library(lubridate)
 library(ggplot2)
-library(data.table)
 library(gamlss)
 library(scoringRules)
 library(depPPR)
@@ -28,15 +27,9 @@ quantile_methods <- c("random", "equally_spaced", "equally_spaced_shift")
 reshuffling_methods <- c("ecc", "ssh", "simssh")
 
 ####################################################################
-################# IMPORT ALL DATA AND POST-PROCESS:
+################# IMPORT DATA:
 ####################################################################
-# define file
-kepsobs_rds <- list.files(
-  path =  paste0(main_dir, "/data/"),
-  pattern = "KEPSOBS_T_2019-2021",
-  full.names = TRUE)
-
-# import forecast data:
+# import forecast data: kepsobs_predictions
 # this is a data.frame containing all hourly observations for all stations in station_names
 # name: station name
 # hlon/hlat: lon/lat from Harmonie
@@ -46,70 +39,30 @@ kepsobs_rds <- list.files(
 # valid_time: datetime in the format "2019-02-10 01:00:00"
 # T: Observed 2m temperature
 # EM000-EM010: Ensemble member forecasts
-kepsobs_data <- readRDS(kepsobs_rds)
-
-# make predictors for EMOS: ensemble mean and SD
-kepsobs_data$EnsM <- apply(kepsobs_data %>% dplyr::select(starts_with("EM")), 1, mean)
-kepsobs_data$EnsS <- apply(kepsobs_data %>% dplyr::select(starts_with("EM")), 1, sd)
+# EnsM / EnsS = ensemble mean and sd
+# mu = predicted mu
+# sigma = predicted sigma
+# y = T2m
 
 # which init_times have the complete data:
-kepsobs_complete_inits <- kepsobs_data %>%
+kepsobs_complete_inits <- kepsobs_predictions %>%
   count(init_time) %>%
   filter(n == length(station_names) * length(lead_times)) %>%
   pull(init_time)
 
-kepsobs_data <- kepsobs_data %>% filter(init_time %in% kepsobs_complete_inits)
+kepsobs_predictions <- kepsobs_predictions %>% filter(init_time %in% kepsobs_complete_inits)
 
 
 
 ####################################################################
-# local PP
-# train on June-July 2020, test on August 2020
+################# IMPORT DATA:
 ####################################################################
-
-train <- kepsobs_data %>%
-  filter(year(init_time) == 2020 & month(init_time) %in% c(6,7))
-test <- kepsobs_data %>%
-  filter(year(init_time) == 2020 & month(init_time) %in% c(8)) # 31 days
-
-# fit a model per station and leadtime:
-# loop over stations:
-fits <- list()
-preds_list <- list()
-for(st in seq_along(station_names)){
-  # loop over leadtimes
-  fits_lt <- list()
-  preds_lt <- list()
-  for(lt in seq_along(lead_times)){
-    # define train/test for each station/leadtime
-    train_stlt <- train %>% filter(name == station_names[st] & leadtime == lead_times[lt])
-    test_stlt <- test %>% filter(name == station_names[st] & leadtime == lead_times[lt])
-
-    # fit the model:
-    #   a normal distribution where we predict mu from EnsM and sigma from EnsS
-    fits_lt[[lt]] <- gamlss(T~EnsM, sigma.formula = ~EnsS,
-                            data = as.data.frame(train_stlt),
-                            family = NO())
-
-    # predict the params and combine with test set
-    preds_lt[[lt]] <- predictAll(fits_lt[[lt]], newdata = test_stlt) %>%
-      as.data.frame() %>%
-      cbind(test_stlt, .)
-  }
-  fits[[st]] <- fits_lt
-  preds_list[[st]] <- rbindlist(preds_lt)
-
-}
-
-preds <- rbindlist(preds_list)
-
-
 # make new ensemble members by drawing from the distributions:
 # draw members: random or equally spaced
 quants <- mapply(FUN = get_quantiles, method = quantile_methods[-1], MoreArgs = list(n_members = length(keps_members)), SIMPLIFY = FALSE)
 
 # for random quants we need a new random draw for each forecast time
-quants$random = lapply(seq_len(nrow(preds)), function(nr){
+quants$random = lapply(seq_len(nrow(kepsobs_predictions)), function(nr){
   mapply(FUN = get_quantiles, method = quantile_methods[1], MoreArgs = list(n_members = length(keps_members)), SIMPLIFY = TRUE) %>%
     t() %>%
     as.data.frame()
