51e45fb1f8c1d6e76fb242dfad8334d026105e60
Kate Saunders <kate.saunders@monash.edu>
2023-06-21 16:03:29 +0200

    Reintroduced a seq by option to the functions

--- R/schaake_date_helpers.R
+++ R/schaake_date_helpers.R
@@ -1,6 +1,12 @@
-get_missing_datetimes <- function(obs_datetime, ...){
+get_missing_datetimes <- function(obs_datetime, by = "hours" ...){
 
-  all_datetimes = seq(min(obs_datetime, na.rm = TRUE), max(obs_datetime, na.rm = TRUE), by = "hours")
+  # need to handle if there is missingness
+  # handle different entry forms, eg. has NAs
+  # need to code for different step times
+
+  all_datetimes = seq(min(obs_datetime, na.rm = TRUE),
+                      max(obs_datetime, na.rm = TRUE),
+                      by = by)
 
   missing_datetimes = setdiff(as.character(all_datetimes),
                               as.character(obs_datetime)) %>%
@@ -13,14 +19,15 @@ get_missing_datetimes <- function(obs_datetime, ...){
 get_nearby_invalid_times <-function(datetime,
                                     window,
                                     init_times = "00",
-                                    return_type = "date"){
+                                    return_type = "date",
+                                    by = "hours"){
 
   # eg. window = days(2)
 
   if(!(return_type %in% c("char", "date")))
     error("Invalid return_type specificied")
 
-  bad_window = seq(datetime - window, datetime, by = "hours")
+  bad_window = seq(datetime - window, datetime, by = by)
   bad_init_times = bad_window[which(hour(bad_window) %in% as.numeric(init_times))]
 
   if(return_type == "char")
@@ -33,13 +40,16 @@ get_nearby_invalid_times <-function(datetime,
 
 get_all_bad_datetimes <- function(missing_datetimes,
                                   window,
-                                  init_times = "00", ...){
+                                  init_times = "00",
+                                  by = "hours",
+                                  ...){
 
   all_bad_datetimes = sapply(missing_datetimes,
                              get_nearby_invalid_times,
                              init_times = init_times,
                              window = window,
-                             return_type = "char") %>%
+                             return_type = "char",
+                             by = by) %>%
     do.call("c", .)
 
   if(all(as.numeric(init_times) == 0)){
@@ -54,11 +64,13 @@ get_all_bad_datetimes <- function(missing_datetimes,
 
 get_good_schaake_datetimes <- function(obs_datetime,
                                        init_times,
-                                       all_bad_datetimes, ...){
+                                       all_bad_datetimes,
+                                       by = "hours",
+                                       ...){
 
   #assumes initiated on the hour
 
-  all_datetimes = seq(min(obs_datetime), max(obs_datetime), by = "hours")
+  all_datetimes = seq(min(obs_datetime), max(obs_datetime), by = by)
 
   init_datetimes = all_datetimes[which(hour(all_datetimes) %in% as.numeric(init_times))]
 
