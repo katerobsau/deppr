2b880df7b95d36fb5b6eb7c88f1e949a701dafdd
Kate Saunders <kate.saunders@monash.edu>
2023-06-21 13:33:13 +0200

    Updated the package documentation

--- R/get_quantiles.R
+++ R/get_quantiles.R
@@ -1,51 +1,58 @@
-#' Creates a vector of quantiles
+#' Quantile sampling
 #'
-#' This function produces qunatiles for ECC-R (random qunatiles),
-#' ECC-Q (uniform  quantiles) or ECC-S (jittered quantiles).
+#' Creates a matrix of sampled quantiles for drawing new ensemble members
 #'
-#' @param n_members number of members in the ensemble (must be an integer)
-#' @param method one of the characters \code{'random'}, \code{'equally_spaced'}, \code{'Q1'},
-#'  or \code{'S'}. This character corresponds to the desired sampling method.
-#'  There are two types of equi-spaced quantiles, 'equally_spaced' and 'Q1'.
-#' @param n_reps ADD DETAILS
+#' @param n_members number of members in the ensemble. This must be an integer.
+#' @param method character string corresponding to the desired quantile sampling method
+#' and must be one of \code{'R'}, \code{'Q'}, \code{'QJ'} or \code{'QH'}.
+#' @param n_reps number of replicates of the function corresponding to the
+#' dimension of the forecast
 
-#' @return a vector of sample quantiles
+#' @return a matrix of sample quantiles where the number of rows corresponds to
+#' n_reps and the number of columns corresponds to n_members
 #'
-#' @details If the \code{method} is \code{'random'} then quantiles are randomly sampled.
-#' If the \code{method} is \code{'equally_spaced'} then qunatiles are equally sampled.
-#' #' If the \code{method} is \code{'Q1'} then quantiles are equally sampled (Bröck et al. 2012)
-#' If the \code{method} is \code{'S'} then the quantiles are jittered.
+#' @details
+#' If the \code{method} is \code{'R'} then the quantiles are randomly sampled.
+#' If the \code{method} is \code{'Q'} then the sampled quantiles are equally spaced.
+#' If the \code{method} is \code{'QH'} then the sampled quantiles are equally spaced using a different sampling scheme.
+#' If the \code{method} is \code{'QJ'} then the sampled quantiles are equally spaced and jittered.
 #'
 #' @author Kate Saunders and Kirien Whan
 #'
-#' @references
-#'
-#' Schefzik, Roman, Thordis L. Thorarinsdottir, and Tilmann Gneiting.
-#' "Uncertainty quantification in complex simulation models using ensemble
-#' copula coupling." Statistical science 28.4 (2013): 616-640.
-#'
-#' Hu, Yiming, et al. "A stratified sampling approach for improved sampling
-#' from a calibrated ensemble forecast distribution." Journal of Hydrometeorology
-#' 17.9 (2016): 2405-2417.
-#'
-#' Bröck et al. 2012
-#'
-#' @seealso \code{\link{get_quantiles}}
+#' @seealso \code{\link{run_shuffle_template}}
 #'
 #' @examples
-#' get_quantiles(n_members = 3, method = "random")
-#' get_quantiles(n_members = 4, method = "equally_spaced")
-#' get_quantiles(n_members = 4, method = "equally_spaced_shift")
-#' get_quantiles(n_members = 5, method = "jittered")
+#'
+#' # Examples of the different sampling methods
+#' get_quantiles(n_members = 3, method = "R")
+#' get_quantiles(n_members = 4, method = "Q")
+#' get_quantiles(n_members = 4, method = "QJ")
+#' get_quantiles(n_members = 5, method = "QH")
+#'
+#' # Examples showing difference in methods for repeat sampling
+#' get_quantiles(n_members = 3, method = "R", n_reps = 2)
+#' get_quantiles(n_members = 3, method = "Q", n_reps = 2)
 #'
 get_quantiles <- function(n_members, method, n_reps = 1){
 
+  if(!is.integer(n_members))
+    error("Number of members, n_members, must be an integer")
+
+  if(!(method %in% c("R", "Q", "QJ", "QH")))
+    error("Quantile sampling method must be one of R, ES, ESJ or ESH. See help.")
+
+  if(!is.integer(n_reps))
+    error("Number of replicates must be an integer")
+
+  if(n_reps < 1)
+    error("Number of replicates must be greater than or equal to 1")
+
   quantiles <- replicate(
                 switch(method,
-                      random = runif(n_members),
-                      equally_spaced = (1:n_members)/(n_members + 1),
-                      equally_spaced_jittered = (1:n_members - 1)/n_members + runif(n_members)/n_members,
-                      equally_spaced_shift = (1:n_members - 0.5)/n_members),
+                      R = runif(n_members),
+                      Q = (1:n_members)/(n_members + 1),
+                      QJ = (1:n_members - 1)/n_members + runif(n_members)/n_members,
+                      QH = (1:n_members - 0.5)/n_members),
                 n = n_reps) %>%
     matrix(., byrow = TRUE, nrow = n_reps)
 
